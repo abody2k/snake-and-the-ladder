6c5cd11986a507ccf53a57f60f528c654aa314a8
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-24 21:59:56 +0300

    Implemented createRoom in redis

--- backend/src/rooms.ts
+++ backend/src/rooms.ts
@@ -5,9 +5,25 @@ import { createClient } from "redis";
 export async function CreateRedisClient() {
 
     let client = await createClient({
-       url:"redis://redis"
+        url: "redis://redis"
     }).connect();
 
     return client;
+
+}
+
+
+
+export async function createRoom(playerID: string) {
+
+    let client = await CreateRedisClient();
+    client.set(`room${playerID}`, JSON.stringify({
+
+        wins: 0,
+        loses: 0,
+        playerTurn:true,
+        playerPos:1,
+        pcPos:1
+    }))
 
 }
