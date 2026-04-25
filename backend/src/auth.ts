1f2c3a2bc3840575269ff90a2cb260c2d3aece23
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-25 09:30:23 +0300

    Fully implemented register account in auth file with hashing

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,6 +1,30 @@
-export async function register({}:{username:string,password:string}){
+import { CreateRedisClient } from "./util.ts";
+import { hash, verify } from "argon2"
+
+/**
+ * creates an account using given username and password
+ * on success the function will run well
+ * on failure it will throw an error
+ * @param param0 username and password as strings
+ */
+export async function register({ username, password }: { username: string, password: string }) {
 
 
-    
+    let client = await CreateRedisClient();
+
+    if (await client.get(username)) { // check if user already exists
+        client.destroy();
+        throw new Error("Username already exist!"); // throw an error
+
+    } else {
+
+        await client.set(username, await hash(password)); // set the username and password
+        client.destroy();
+    }
+
+
+
+
+
 
 }
