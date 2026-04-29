9ec2d41c803f6cac87222702ed62bab1811f87c3
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-29 11:06:17 +0300

    Implemented basic socket class

--- test/api/socket-client.ts
+++ test/api/socket-client.ts
@@ -1,5 +1,20 @@
+import { io } from "socket.io-client"
+
 /**
  * a helper class to make connecting to sockets and sending
  * and handling data over sockets easier
  */
 
+export class SocketIoClient {
+
+    socket
+    constructor() {
+
+        this.socket = io("http://app:3000")
+
+    }
+
+
+
+
+}
