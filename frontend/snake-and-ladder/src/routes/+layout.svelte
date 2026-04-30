d4707c4a764c0727010c0be54e593b2cfba5bd82
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-30 10:29:52 +0300

    Moved socket file to /lib and initiated the socket connection.

--- frontend/snake-and-ladder/src/routes/+layout.svelte
+++ frontend/snake-and-ladder/src/routes/+layout.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+    import type { Socket } from 'socket.io-client';
+    import { onMount } from 'svelte';
+    import { getSocket } from '$lib/socket';
+	let socket : Socket
 	let { children } = $props();
+
+	onMount(()=>{
+
+		socket = getSocket();
+		if (!socket.connected){
+
+			socket.connect()
+		}
+		//HANDLE INCOMING TRANSMISSIONS OVER HERE
+	})
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
