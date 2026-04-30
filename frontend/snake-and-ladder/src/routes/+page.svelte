5036af77afd67469eef7985881fa4ffe813e2b3a
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-30 18:21:51 +0300

    Modified playAgainstAI and made it playAgainst to make it work for both AI and Humans

--- frontend/snake-and-ladder/src/routes/+page.svelte
+++ frontend/snake-and-ladder/src/routes/+page.svelte
@@ -11,12 +11,13 @@
     import Topbar from "./Topbar.svelte";
     import { goto } from "$app/navigation";
 
-    async function playAgainstAI() {
+    async function playAgainst(target : "AI" | "HUMAN") {
         //if not logged in then login
         let userID = localStorage.getItem("userID");
         if (!userID) {
             await goto("/auth");
         } else {
+            localStorage.setItem("target",target);
             await goto(`/rooms/${userID}`);
         }
     }
@@ -27,10 +28,10 @@
     <div class="flex justify-center items-center self-center">
         <Listgroup>
             <ListgroupItem>
-                <Button outline color="dark" onclick={playAgainstAI}>Play against AI</Button>
+                <Button outline color="dark" onclick={async ()=>{await playAgainst("AI")}}>Play against AI</Button>
             </ListgroupItem>
             <ListgroupItem
-                ><Button outline color="dark">Play with friends</Button>
+                ><Button outline color="dark" onclick={async ()=>{await playAgainst("HUMAN")}}>Play with friends</Button>
             </ListgroupItem>
         </Listgroup>
     </div>
