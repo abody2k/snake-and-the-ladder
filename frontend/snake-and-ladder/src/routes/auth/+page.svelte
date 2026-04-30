b370dd5eeb5b956fba81ac6a093a74903c826dcc
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-30 17:52:43 +0300

    Implemented both sign in and signup functions in auth UI

--- frontend/snake-and-ladder/src/routes/auth/+page.svelte
+++ frontend/snake-and-ladder/src/routes/auth/+page.svelte
@@ -6,6 +6,8 @@
         Label,
         Listgroup,
         ListgroupItem,
+        Modal,
+        Spinner,
     } from "flowbite-svelte";
     import Topbar from "../Topbar.svelte";
     import { handleAuthData, postRequest } from "../../utils";
@@ -17,31 +19,57 @@
 
     let username = $state("");
     let password = $state("");
+    let busy = $state(false);
 
     async function loginRequest() {
         let data;
-
+        busy = true;
         try {
             data = await postRequest("login", {
                 username: username,
                 password: password,
             });
 
-            await handleAuthData(data,username);
-            
+            await handleAuthData(data, username);
+
             goto("/");
         } catch (error) {
             console.log(error);
         }
+
+        busy = false;
     }
 
+    async function signupRequest() {
+        let data;
+        busy = true;
+        try {
+            data = await postRequest("register", {
+                username: username,
+                password: password,
+            });
 
+            await handleAuthData(data, username);
 
-    
+            goto("/");
+        } catch (error) {
+            console.log(error);
+        }
+
+        busy = false;
+    }
 </script>
 
 <Topbar></Topbar>
-
+<Modal
+    class="flex items-center justify-center  size-fit"
+    form
+    title={login ? "Logging in" : "Signing up"}
+    open={busy}
+    permanent
+>
+    <Spinner size="12" type="pulse"></Spinner>
+</Modal>
 <div class="flex justify-center items-center">
     <Listgroup>
         <ListgroupItem>
@@ -54,11 +82,11 @@
         </ListgroupItem>
         {#if login}
             <ListgroupItem>
-                <Button color="dark">Login</Button>
+                <Button color="dark" onclick={loginRequest}>Login</Button>
             </ListgroupItem>
         {:else}
             <ListgroupItem>
-                <Button color="dark">Sign up</Button>
+                <Button color="dark" onclick={signupRequest}>Sign up</Button>
             </ListgroupItem>
         {/if}
 
