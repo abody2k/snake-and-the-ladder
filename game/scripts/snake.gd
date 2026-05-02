761c1fa770fea0eb4acd290d4416a0026640fef3
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-02 14:14:21 +0300

    Updated the setupCurve script

--- game/scripts/snake.gd
+++ game/scripts/snake.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+
+func setupCurve(start: Vector3, ending:Vector3):
+	
+	var distance = ending.distance_to(start)
+	var steps = distance/float($Skeleton2D/Head.get_child_count(true)-1)
+	
