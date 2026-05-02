8817c90272d5a9d831ab96bd532dccdf35aa6c82
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-02 17:39:10 +0300

    Added player piece, set up traps and ladder

--- game/scripts/snake.gd
+++ game/scripts/snake.gd
@@ -1,8 +1,27 @@
 extends Node2D
 
 
-func setupCurve(start: Vector3, ending:Vector3):
+func setupCurve(start: Vector2, ending:Vector2):
 	
-	var distance = ending.distance_to(start)
-	var steps = distance/float($Skeleton2D/Head.get_child_count(true)-1)
+	print([start,ending])
+	var distance = start.distance_to(ending)
+	var steps = 18
+	var segment_length = distance/float(steps-1)
+	var dir = (ending - start).normalized()
+	#$Skeleton2D/Head.transform.origin = ending
+	#$Skeleton2D/Head.rotation = dir.angle()
 	
+	
+	for i in range(1,steps+1):
+		
+		find_child(str(i)).transform.origin = start + (dir * (i * segment_length))
+		find_child(str(i)).rotation = dir.angle()
+func _ready():
+	
+	pass
+	
+	
+
+
+func _on_timer_timeout():
+	setupCurve(Vector2.ZERO, Vector2.ONE* randf_range(200,340))
