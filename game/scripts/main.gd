3b8f9803846639eec9d862dfa064cd6f7d4dea62
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-02 19:27:29 +0300

    Connected game_updated callback to the js window gameUpdated

--- game/scripts/main.gd
+++ game/scripts/main.gd
@@ -5,11 +5,12 @@ extends Node2D
 func _ready():
 	var window_js =JavaScriptBridge.get_interface("window")
 	window_js.userJoined = JavaScriptBridge.create_callback(user_joining_game)
+	window_js.gameUpdated = JavaScriptBridge.create_callback(game_updated)
 	
 
 func user_joining_game(args):
 	pass
 	
 
-func game_updated():
+func game_updated(args):
 	pass
