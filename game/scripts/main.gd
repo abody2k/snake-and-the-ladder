66c730d0563fc585ab92a2989d81cdd40e1cb54d
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-02 19:26:49 +0300

    Implemented basic logic for user_joining_game

--- game/scripts/main.gd
+++ game/scripts/main.gd
@@ -2,9 +2,14 @@ extends Node2D
 
 
 
-func user_joining_game():
+func _ready():
+	var window_js =JavaScriptBridge.get_interface("window")
+	window_js.userJoined = JavaScriptBridge.create_callback(user_joining_game)
+	
+
+func user_joining_game(args):
 	pass
 	
 
-func game_update():
+func game_updated():
 	pass
