74443578dfb894edec3cb72c85bb205efd03bf42
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-04 10:32:19 +0300

    splited tests into auth and pages

---
+++ test/tests/auth.spec.ts
@@ -0,0 +1,43 @@
+import { expect, test } from "../fixtures/globalFixture";
+import { login, register } from "./flows/auth.flows";
+
+
+
+test.describe("Auth tests goes here", () => {
+
+
+
+    test("Signs up with new valid credentials", async ({ page, home, auth }) => {
+
+
+        await home.goHome();
+        await home.clickOnAuthInTopBar();
+        await register("a very new username", "cool password", auth);
+
+        await expect(page).toHaveURL(process.env.BASE_URL);
+
+
+    })
+
+
+    test("Logs in when valid credentials are provided", async ({ page, home, auth }) => {
+
+
+        await home.goHome();
+        await home.clickOnAuthInTopBar();
+        await register("nneww username", "pass", auth);
+
+        await home.clickOnAuthInTopBar()
+        await auth.clickOnLogOut();
+        await login("nneww username", "pass", auth);
+
+        await expect(page).toHaveURL(process.env.BASE_URL);
+
+
+    })
+
+
+    
+
+
+})
--- test/tests/pages.spec.ts
+++ test/tests/pages.spec.ts
@@ -48,37 +48,5 @@ test.describe("UI tests go here", () => {
     })
 
 
-    test("Signs up with new valid credentials", async ({ page, home, auth }) => {
-
-
-        await home.goHome();
-        await home.clickOnAuthInTopBar();
-        await register("a very new username", "cool password", auth);
-
-        await expect(page).toHaveURL(process.env.BASE_URL);
-
-
-    })
-
-
-    test("Logs in when valid credentials are provided", async ({ page, home, auth }) => {
-
-
-        await home.goHome();
-        await home.clickOnAuthInTopBar();
-        await register("nneww username", "pass", auth);
-
-        await home.clickOnAuthInTopBar()
-        await auth.clickOnLogOut();
-        await login("nneww username", "pass", auth);
-
-        await expect(page).toHaveURL(process.env.BASE_URL);
-
-
-    })
-
-
-    
-
 
 })
